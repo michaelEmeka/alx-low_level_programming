d1624232c5eea21919ef9c114280cf0262bc4842
michaelEmeka <mikelonu15@gmail.com>
2022-07-01 15:14:36 -0700

    Add 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints numbers 0 - 9
+ * Description: prints a new line after this iteration
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%i", n);
+	}
+	putchar('\n');
+	return (0);
+}
