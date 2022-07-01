3fdef6c0ede458ba70c95f24a1f56be7e0ab32b1
michaelEmeka <mikelonu15@gmail.com>
2022-07-01 14:19:13 -0700

    Add 4-alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints the alphabet in lowercase followed by a new line
+ * Descriprion: excludes alphabet q and e
+ * Return: 0
+ */
+int main(void)
+{
+	int a, i;
+
+	a = 'a';
+	for (i = 0; i < 26; i++)
+	{
+		if (a != 'q' && a != 'e')
+			putchar(a);
+	}
+	putchar('\n');
+
+	return (0);
+}
