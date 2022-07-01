8d433e366d5c696c9fcd20ea66ad1e4ce7e7ea9a
michaelEmeka <mikelonu15@gmail.com>
2022-07-01 08:31:59 -0700

    Create folder and Add 0-positive_or_negative.c

---
+++ 0x01-variable_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - main block
+ * Description: Get a random number and print the number
+ * and if it is positive, negative, or zero
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%lu is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%lu is negative\n", n);
+	}
+	else
+	{
+		printf("%lu is zero\n", n);
+	}
+	return (0);
+}
---
+++ 0x01-variable_if_else_while/README.md
@@ -0,0 +1,1 @@
+This is a directory containing answer files to the c if_else tasks
