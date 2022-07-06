54d807baee98f3b2de26340709ef5b67a7089856
michaelEmeka <mikelonu15@gmail.com>
2022-07-06 15:28:00 -0700

    Add 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - main block
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
