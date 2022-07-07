610d4b1ecd9ea8189828563dd2050bdb4e303dd7
michaelEmeka <mikelonu15@gmail.com>
2022-07-07 07:14:47 -0700

    Add 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - main function
+ * Description: this program prints numbers 0-9 with exceptionof 2 and 4
+ * Return: nil
+ */
+void print_most_numbers(void)
+{
+	int n = 0;
+
+	for (; n < 10; n++)
+	{
+		if (n != 2 && n != 4)
+		{
+			_putchar('0' + n);
+		}
+	}
+}
