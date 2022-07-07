9d504c66b1d25d601e2883e91658e34c2d74c2a8
michaelEmeka <mikelonu15@gmail.com>
2022-07-06 17:41:15 -0700

    Modify 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,10 +8,18 @@
  */
 int print_last_digit(int n)
 {
+	int last_digit;
+
 	if (n < 0)
 	{
-		n = n * (-1);
+		last_digit = (-1 * (n % 10));
+		_putchar (last_digit + '0');
+		return (last_digit);
 	}
-	_putchar('0' + (n % 10));
-	return (n % 10);
+	else
+	{
+		last_digit = (n % 10);
+		_putchar (last_digit + '0');
+		return (last_digit);
+	}
 }
