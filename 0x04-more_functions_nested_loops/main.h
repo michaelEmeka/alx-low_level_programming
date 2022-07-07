8fe3fb27c14dc811659aa22f0341baa24e11a173
michaelEmeka <mikelonu15@gmail.com>
2022-07-07 08:46:38 -0700

    Modify 5

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -5,3 +8,5 @@ int _putchar(char c);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
+
+#endif
