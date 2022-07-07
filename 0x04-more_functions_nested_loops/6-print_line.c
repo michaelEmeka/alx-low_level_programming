a7555957be585748f2fd16c0c39ae76ea3efe217
michaelEmeka <mikelonu15@gmail.com>
2022-07-07 09:08:26 -0700

    Add 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - main function
+ * Description: This function prints a line
+ * @n: n times to print '_'
+ * Return: nil
+ */
+void print_line(int n)
+{
+	if (n > 0 && n != 0)
+	{
+		while (--n)
+		{
+			_putchar('_');
+
+		}
+	}
+	_putchar('\n');
+}
