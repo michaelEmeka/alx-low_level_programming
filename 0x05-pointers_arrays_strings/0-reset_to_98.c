dcc0eeb744e4da109b88125e1096e478eb549aff
michaelEmeka <mikelonu15@gmail.com>
2022-07-12 12:37:48 -0700

    Create 0x05-pointers_arrays_strings and 0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - chek the code
+ *
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This folder contains answer files to the c pointers tasks
