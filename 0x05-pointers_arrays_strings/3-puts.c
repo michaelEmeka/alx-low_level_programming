d356835c258e743a8856263b854b948a7265a4ca
michaelEmeka <mikelonu15@gmail.com>
2022-07-13 05:57:40 -0700

    Add 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - this function prints a string followed by a new line
+ * @str: string type parameter
+ * Return: Always return 0;
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (i < _strlen(s))
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
