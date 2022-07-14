4bd1f87634473801b137e226a93a0724a72fb2fb
michaelEmeka <mikelonu15@gmail.com>
2022-07-13 21:28:22 -0700

    Add 6-puts.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts - main function
+ * @str: string argument
+ * Return: Always 0;
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i];
+		i++;
+	}
+	_putchar('\n');
+}
