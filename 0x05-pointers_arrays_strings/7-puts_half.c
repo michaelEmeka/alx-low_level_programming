701063fd107abe13332de3be8614668c0486c436
michaelEmeka <mikelonu15@gmail.com>
2022-07-13 22:04:26 -0700

    dd 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts_half - main function
+ * @str: function's parameter
+ * Return: Always 0
+ */
+void puts_half(char *str)
+{
+	int i = 0, b = 0, halflen;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if (i % 2 != 0)
+	{
+		halflen = (i - 1) / 2;
+		_putchar(str[halflen]);
+	}
+	else
+	{
+		halflen = i / 2;
+		while (str[b] != '\0')
+		{
+			_putchar(str[halflen]);
+			halflen++;
+		}
+	}
+}
