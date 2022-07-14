1c4608f54c82151d1c687825f84eeee1a16888ff
michaelEmeka <mikelonu15@gmail.com>
2022-07-14 05:20:36 -0700

    Add 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strcat - main function
+ * @dest: first argument
+ * @src: second parameter
+ * Return: Always 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	return (dest);
+}
