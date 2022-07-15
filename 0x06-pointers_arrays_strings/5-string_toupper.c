a2d26f91a7e3958db6d6300fad326e3dac5fe2ca
michaelEmeka <mikelonu15@gmail.com>
2022-07-15 02:13:19 -0700

    Add 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *string_toupper - converts strings to uppercase
+ * @str: string parameter
+ * Return: always 0
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = ('0' + 32);
+		}
+		i++;
+	}
+
+	return (str);
+}
