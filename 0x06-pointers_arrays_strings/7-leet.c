a3a318c236a3674c4294fb4b1428e20e52df9960
michaelEmeka <mikelonu15@gmail.com>
2022-07-15 06:54:04 -0700

    Add 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *leet - encoding function
+ * @str: striing parameter
+ * Return: encoded string
+ */
+char *leet(char *str)
+{
+	int a = 0, i;
+	char key[5] = {'a', 'e', 'o', 't', 'l'};
+	char val[5] = {4, 3, 0, 7, 1};
+
+	while (str[a] != '\0')
+	{
+		while (i < 5)
+		{
+			if (str[a] == key[i] || str[a] == key[i] - 32)
+			{
+				str[a] = val[i];
+			}
+			i++;
+		}
+		a++;
+	}
+	return (str);
+}
