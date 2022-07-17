feea32e794faeda53f501d14192e01b9a86a025a
michaelEmeka <mikelonu15@gmail.com>
2022-07-17 12:19:47 -0700

    Modify 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,17 +17,19 @@ void times_table(void)
 			tens = (p - units) / 10;
 			if (p < 10)
 			{
+				if (y != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 				_putchar('0' +  p);
 			}
 			else
 			{
-				_putchar('0' + tens);
-				_putchar('0' + units);
-			}
-			if (y != 9)
-			{
 				_putchar(',');
 				_putchar(' ');
+				_putchar('0' + tens);
+				_putchar('0' + units);
 			}
 		}
 		_putchar('\n');
