168e6c7daa95975da3594f3d1e4208bfc914aff3
michaelEmeka <mikelonu15@gmail.com>
2022-07-18 04:36:15 -0700

    Add 1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memcpy - this function copies  bytes from memory area src to dest
+ * @dest: destinatiion parameter
+ * @src: source parameter
+ * @n: number of bytes
+ * Return: a pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+	while(i < n)
+	{
+		dest + i = src[i];
+	}
+	return (dest);
+}
