ef30e90bcbedb01ada391cd6a20e70d8b4b6debd
michaelEmeka <mikelonu15@gmail.com>
2022-07-19 09:14:03 -0700

    Modify 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,14 +10,20 @@
 char *_strchr(char *s, char c)
 {
 	int i = 0;
+	char *p = NULL;
 
 	while (s[i] != '\0')
 	{
 		if (s[i] == c)
 		{
+			p = s + i;
 			break;
+		}
+		else
+		{
+			p = NULL;
 		}
 		i++;
 	}
-	return (s + i);
+	return (p);
 }
