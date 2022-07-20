5500254095d3263614f8bee85f9b056ae5fd29ce
michaelEmeka <mikelonu15@gmail.com>
2022-07-20 14:41:24 -0700

    Modify 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,9 +9,13 @@
  */
 int _pow_recursion(int x, int y)
 {
-	if (y <= 1)
+	if (y == 0)
 	{
-		return (x);
+		return (1);
+	}
+	if (y < 0)
+	{
+		return (-1);
 	}
 	return (_pow_recursion(x, y - 1) * x);
 }
