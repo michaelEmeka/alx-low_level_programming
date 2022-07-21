349228c61b1e29be6c9dc912841473020eeb01b9
michaelEmeka <mikelonu15@gmail.com>
2022-07-21 13:48:16 +0100

    Modify 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,6 +2,8 @@
 
 /**
  * main -tests function that prints if integer is positive or negative
+ * positive_or_negative - checks if a number is
+ * positive or negative
  * Return: 0
  */
 int main(void)
