ee559799e31dcd7e9ec88567f9ac412821b26540
michaelEmeka <mikelonu15@gmail.com>
2022-07-24 21:07:33 +0100

    Modify 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,14 +10,18 @@ int main(void)
 
 	while (i < 100)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("%s ", "FizzBuzz");
+		}
+		else if (i % 3 == 0)
 		{
 			printf("%s ", "Fizz");
 		}
-		else if (i % 5 == 0)
+		else if (i % 3 == 0)
 		{
 			printf("%s ", "Buzz");
-		
+		}
 		else
 		{
 			printf("%d ", i);
