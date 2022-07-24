07bca06610ed49e86e5fe5b8f2e4e03a4de289b8
michaelEmeka <mikelonu15@gmail.com>
2022-07-24 19:32:28 +0100

    Add 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: 0
+ */
+int main(void)
+{
+        long int i = 0, a = 1, b = 2, nxtn;
+
+        printf("%ld, %ld", a, b);
+        while (i < 96)
+        {
+                nxtn = a + b;
+                printf(", %ld", nxtn);
+                a = b;
+                b = nxtn;
+                i++;
+        }
+        putchar('\n');
+        return (0);
+}
