7924686a8b81ef9c27e86f1e71f7591f0320d1ce
michaelEmeka <mikelonu15@gmail.com>
2022-07-24 19:25:10 +0100

    Add 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: 0
+ */
+int main(void)
+{
+	long int a = 1, b = 2, nxtn = 0, sum = 0;
+
+	sum +=b;
+	while (nxtn <= 4000000)
+	{
+		nxtn = a + b;
+		a = b;
+		b = nxtn;
+		if (nxtn % 2 == 0)
+		{
+			sum += nxtn;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
