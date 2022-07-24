952ca129cd184d261fcf7267b20cf9ca546b6c6a
michaelEmeka <mikelonu15@gmail.com>
2022-07-24 18:08:45 +0100

    Add 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: 0
+ */
+int main(void)
+{
+	int sum = 0, i = 0;
+
+	while (i < 1024)
+	{
+		if (i % 3 || i % 5)
+		{
+			sum += i;
+		}
+		i++;
+	}
+	printf("%d\n", i);
+
+	return (0);
+}
