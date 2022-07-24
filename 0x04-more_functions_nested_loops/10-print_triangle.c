149b03be2928da80865f144eee1e38324ef9eeb4
michaelEmeka <mikelonu15@gmail.com>
2022-07-24 21:54:16 +0100

    Add 10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_triangle - this function prints a triangle
+ * @size: argument
+ * Return: 0
+ */
+void print_number(int n)
+{
+	int i = 1, s = 0, c = 0;
+
+	if (n > 0)
+	{
+		while (i <= n)
+		{
+			s = n - i;
+			while (s > 0)
+			{
+				_putchar(' ');
+				s--;
+			}
+			c = i;
+			while (c > 0)
+			{
+				_putchar('#');
+				c--;
+			}
+			_putchar('\n');
+			i++;
+		}
+		
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
