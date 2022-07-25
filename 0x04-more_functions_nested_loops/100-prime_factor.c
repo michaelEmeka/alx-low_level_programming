80953c0fafc73eb8b3c395a07724516dd65ed753
michaelEmeka <mikelonu15@gmail.com>
2022-07-25 07:16:08 +0100

    Add 100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: 0
+ */
+int main(void)
+{
+	long long num = 612852475143, i = 2, p = 1, c = 0;
+
+	while (i < num)
+	{
+		if (num % i == 0)
+		{
+			while (p <= i)
+			{
+				if (p % i == 0)
+				{
+					c++;
+				}
+				p++;
+			}
+			if (c == 2)
+			{
+				printf("%lld");
+			}
+		}
+		p = 1;
+		c = 0;
+		i++;
+	}
+	return (0);
+}
