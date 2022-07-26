9791ba4a7fb92bb8afab8102250795b01d7536f5
michaelEmeka <mikelonu15@gmail.com>
2022-07-26 08:49:27 -0700

    Modify 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,9 +6,10 @@
  * @argc: arguments count
  * @argv: arguments vector
  * exit: EXIT_SUCCESS
+ * Return: 0
  */
 int main(int argc, char **argv)
 {
-	printf("%s\n", argv[argc-argc]);
+	printf("%s\n", argv[argc - argc]);
 	exit(EXIT_SUCCESS);
 }
