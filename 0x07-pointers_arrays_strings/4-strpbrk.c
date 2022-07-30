5fb41cd8a72aec28520e30a836bbfc5451506a68
michaelEmeka <mikelonu15@gmail.com>
2022-07-30 15:19:13 +0100

    Add 4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strspn - this function returns the number of bytes in the
+ * initial segment of *s that contains *accept
+ * @s: reference string pointer
+ * @accept: search key
+ * Return: pointer to first appearance
+ */
+char _strpbrk(char *s, char *accept)
+{
+	int i, a;
+
+	i = a = 0;
+	while (*s)
+	{
+		while (accept[a] != '\0')
+		{
+			if (s[i] == accept[a])
+			{
+				return (s);
+			}
+			a++;
+		}
+		a = 0;
+		i++;
+		s++;
+	}
+	return (NULL);
+}
