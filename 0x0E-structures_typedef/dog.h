47a4e125258feaac2fb1d15af6b822cd0acdb5b3
michaelEmeka <mikelonu15@gmail.com>
2022-08-01 22:38:15 +0100

    Modify dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE dog.h
+/**
+ * struct dog - Define a new struct dog with name, age, owner
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ **/
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 }
+
+#endif
