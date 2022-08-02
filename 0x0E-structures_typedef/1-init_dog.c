67300005b33fffaa165a116b63366034a10e41c9
michaelEmeka <mikelonu15@gmail.com>
2022-08-02 11:38:50 +0100

    modify dog.h

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,4 @@
+#include <stdlib.h>
+
+/**
+ * struct dog
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE dog.h
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - Define a new struct dog with name, age, owner
  * @name: name of the dog
@@ -12,5 +12,7 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
