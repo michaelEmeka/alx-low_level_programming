6e0eb384550a7cea5fb03ec80f0c75d2636f2344
michaelEmeka <mikelonu15@gmail.com>
2022-08-03 14:31:45 +0100

    Add 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,13 +11,37 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t d;
-	char *n = name, *o = owner:
+	dog_t *d;
+	int Ln, Lo, i;
 
-	d.name = n;
-	d.age = age;
-	d.owner = o;
+	//allocates memory for d
+	d = (dog_t *)malloc(sizeof(dog_t));
+	//checks if allocation was successful
 	if (d == NULL)
 		return (NULL);
+	Ln = Lo = 0;
+	//get length of name and owner pointers
+	while (name[Ln++])
+		;
+	while (owner[Lo++])
+		;
+	//allocates memory for name and owner elements
+	d->name = malloc(sizeof(d->name) * Ln);
+	if (d->name == NULL)
+	{
+		free (d);
+		return (NULL);
+	}
+	for (i = 0; i <= Ln; i++)
+		d->name[i] = name[i];
+	d->age = age;
+	d->owner = malloc(sizeof(d->owner) * Lo);
+	if (d->name == NULL)
+	{
+		free (d->name);
+		free (d);
+	}
+	for (i = 0; i <= Lo; i++)
+		d->owner[i] = owner[i];
 	return (d);
 }
