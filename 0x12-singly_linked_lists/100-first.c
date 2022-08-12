da1bfaf19089b555c7f76e610c63727a27ae998a
michaelEmeka <mikelonu15@gmail.com>
2022-08-13 00:14:51 +0100

    Modify 100 and lists.h

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
+#include "lists.h"
 /**
  * startupFunc - this function is executed before main
  *
  * Return: nothing
  */
-void startupFunc (void) __attribute__ ((constructor));
 void startupFunc()
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
