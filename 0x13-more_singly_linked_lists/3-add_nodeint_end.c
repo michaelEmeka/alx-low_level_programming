7cb47043092847ffcf70b74573ac467408034601
michaelEmeka <mikelonu15@gmail.com>
2022-08-16 18:49:52 +0100

    Modify 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -16,6 +16,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		return (NULL);
 	end->n = n;
 	end->next = NULL;
+	if (*head == NULL)
+		*head = end;
 	temp = *head;
 	while (temp != NULL)
 		temp = temp->next;
