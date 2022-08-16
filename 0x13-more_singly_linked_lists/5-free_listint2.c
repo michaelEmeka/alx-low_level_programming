221be1235114fd7204d3f96ed4221621e800b590
michaelEmeka <mikelonu15@gmail.com>
2022-08-16 19:24:45 +0100

    Modify 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,7 +9,7 @@ void free_listint2(listint_t **head)
 {
         listint_t *current;
 
-        while (head != NULL)
+        while (*head != NULL)
         {
                 current = *head;
                 *head = *head->next;
