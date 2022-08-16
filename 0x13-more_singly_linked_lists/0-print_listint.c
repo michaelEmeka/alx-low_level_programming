75cf7f35300d80793ac3a2b98ffc702cf0ec622e
michaelEmeka <mikelonu15@gmail.com>
2022-08-16 09:36:47 +0100

    Modify 0-print_listint.c and lists.h

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,10 +10,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t i = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		h = h->next, i++;
+		h = h->next;
+		i++;
 	}
 	return (i);
 }
