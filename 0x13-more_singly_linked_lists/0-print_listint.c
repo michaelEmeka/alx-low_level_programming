adbdb252b09685ab86f25ae1bace1faa6c306105
michaelEmeka <mikelonu15@gmail.com>
2022-08-16 09:25:57 +0100

    Modify 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,12 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t i;
+	size_t i = 0;
 
 	while (h)
 	{
 		printf("%d", h->n);
-		h = h->next;
-		i++;
+		h = h->next, i++;
 	}
 	return (i);
 }
