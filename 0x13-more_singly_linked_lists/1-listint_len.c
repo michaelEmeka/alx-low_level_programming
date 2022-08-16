3e6f9b1fdce74ef4d8cd4b66ce40b70c155ec6b9
michaelEmeka <mikelonu15@gmail.com>
2022-08-16 10:03:21 +0100

    Add 1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * listint_len - this function returns the number of element
+ * in a listint_t
+ * @h: a sample node
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t nElem = 0;
+
+	while (*h++)
+	{
+		nElem++;
+	}
+	return (nElem);
+}
