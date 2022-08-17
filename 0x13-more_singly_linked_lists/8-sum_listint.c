cd16a7f942b4b88b205dd6a484e0f3bd221a1fa9
michaelEmeka <mikelonu15@gmail.com>
2022-08-17 07:47:29 +0100

    Add 8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * sum_listint - this funtion gets the sum of all n's in nodes
+ * @head: head node
+ * Returns: the sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
