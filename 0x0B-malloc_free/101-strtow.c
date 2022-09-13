50eb900ef502bca3bc34047f0a16cea5a03eab01
michaelEmeka <mikelonu15@gmail.com>
2022-09-13 08:56:57 +0100

    Add 101-strtow.c

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * strtow - this function divides a string into separate words.
+ * @str: string literal.
+ * Return: an array of strings representing words extracted.
+ */
+
+char **strtow(char *str);
+{
+	char **words;
+	int i = 0, sizes[100], letters = 0, wrd = 0, j = 0;
+	if (!str)
+		return (NULL);
+	while (str[i])
+	{
+		if (str[i] != ' ')
+		{
+			letters++;
+			if (str[i + 1] == ' ' || !str[i + 1])
+			{
+				sizes[wrd] = letters;
+				wrd++;
+				letters = 0;
+			}
+		}
+		i++;
+	}
+	words = malloc(wrd * sizeof(char *));
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] != ' ')
+		{
+			words[j] = malloc(sizeof(char) * sizes[j]);
+			while(letters < sizes[j])
+			{
+				words[j][letters] = str[i];
+				letters++, i++;
+			}
+			letters = 0, j++;
+		}
+		else
+			i++;
+	}
+	return (words);
+}
