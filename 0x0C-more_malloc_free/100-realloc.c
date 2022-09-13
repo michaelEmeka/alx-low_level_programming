f02e51c4c85296138e79dab5d460366ddd75e398
michaelEmeka <mikelonu15@gmail.com>
2022-09-13 12:49:45 +0100

    Add 100-realloc.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _realloc - this function reallocates memory.
+ * @ptr: string.
+ * @old_size: initial size of ptr.
+ * @new_size: size to be expanded to.
+ * Return: ptr with size new_size.
+ * Or NULL if:
+ * -malloc fails.
+ * -new_size = 0 and ptr != NULL
+ * Or ptr if:
+ * -new_size == old_size
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *temp;
+	int position = 0;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (!new_size && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	temp = malloc(new_size);
+	if (!temp)
+		return (NULL);
+	if (!ptr)
+		return (temp);
+
+	while (position < old_size)
+	{
+		temp[position] = ptr[position];
+		position++;
+	}
+	free(ptr);
+	return (temp);
+}
