1b584e5fdb8ffdcfacc461ec192629c188f166f0
michaelEmeka <mikelonu15@gmail.com>
2022-09-13 09:32:19 +0100

    Modify 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -11,7 +11,7 @@ char **strtow(char *str)
 	char **words;
 	int i = 0, sizes[100], letters = 0, wrd = 0, j = 0;
 
-	if ((*str == ' ' && *(str + 1) == '\0') || *str == '\0' || str == NULL)
+	if (!*str || !str || (str[0] == ' ' && !str[1]))
 		return (NULL);
 	while (str[i])
 	{
