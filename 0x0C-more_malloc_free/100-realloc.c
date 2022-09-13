c2322556c939fe69d076262741e24aa6f2c225ba
michaelEmeka <mikelonu15@gmail.com>
2022-09-13 12:54:05 +0100

    Modify 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -29,7 +29,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	if (!ptr)
 		return (temp);
-	(void*)ptr;
+	(char*)ptr;
 	while (position < old_size)
 	{
 		temp[position] = ptr[position];
