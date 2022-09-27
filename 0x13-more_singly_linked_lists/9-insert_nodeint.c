ceaee59c16d777169915dabee841a014bd96f3c8
michaelEmeka <mikelonu15@gmail.com>
2022-09-27 13:53:54 +0100

    Modify file 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,7 +32,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			temp = temp->next;
 			idx--;
 		}
-		if (!prev->next)
+		if (!temp)
 			return (NULL);
 		prev->next = new;
 		new->next = temp;
