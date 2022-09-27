4466242302b13c86eca670cae37792a08482f2c1
michaelEmeka <mikelonu15@gmail.com>
2022-09-27 14:07:56 +0100

    Modify 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,12 +26,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	else
 	{
-		while (idx > 0)
+		while (idx > 0 && temp)
 		{
 			prev = temp;
 			temp = temp->next;
 			idx--;
 		}
+		if (!temp)
+			return (NULL);
 		prev->next = new;
 		new->next = temp;
 	}
