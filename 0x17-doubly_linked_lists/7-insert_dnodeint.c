45eb4ae92685643019f85161a586c765fca3f06c
michaelEmeka <mikelonu15@gmail.com>
2022-10-14 12:20:32 +0100

    Modify file 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -32,7 +32,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (!temp)
 			return (NULL);
 	}
-	//tprev = temp->prev;
+	/*tprev = temp->prev;*/
 	temp->prev = new;
 	new->next = temp;
 	new->prev = tprev;
