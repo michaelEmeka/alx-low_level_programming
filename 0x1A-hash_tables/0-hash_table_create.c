286e5e4c7a52d87109804db41f701ed895472580
michaelEmeka <mikelonu15@gmail.com>
2022-10-26 15:38:25 +0100

    Modify file 0.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,13 +8,18 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
+	hash_node_t **arr;
 
 	new_table = malloc(sizeof(hash_table_t));
 
-	if (new_table)
+	if (!new_table)
 		return (NULL);
 	new_table->size = size;
-	new_table->hash_node_t = NULL;
+
+	arr = malloc(sizeof(hash_node_t) * size);
+	if (!arr)
+		return (NULL);
+	new_table->array = arr;
 
 	return (new_table);
 }
