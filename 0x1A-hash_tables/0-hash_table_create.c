2d03ef4bafa3ae93dab9b435f96ed7833d7090ce
michaelEmeka <mikelonu15@gmail.com>
2022-11-21 05:48:08 +0100

    Modify all files except readme

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	new_table->size = size;
 
-	new_table->array = malloc(sizeof(hash_node_t) * size);
+	new_table->array = malloc(sizeof(hash_node_t*) * size);
 	if (!(new_table->array))
 		return (NULL);
 	while (i < size)
